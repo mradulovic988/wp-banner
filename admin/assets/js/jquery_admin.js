ae855cc5651715a190a14ae31ad05ce196cd6672
Marko <upss070288@gmail.com>
2020-09-19 11:21:34 +0200

    Fixed issue regarding not showing managing part with JS

--- admin/assets/js/jquery_admin.js
+++ admin/assets/js/jquery_admin.js
@@ -2,19 +2,21 @@ jQuery(document).ready(function( $ ) {
     $(document).click(function() {
 
         if ($("input#customization").is(':checked')) {
+            $("tr.wp_banner_class_managing").show();
             $("tr.wp_banner_class_templates").hide();
             $("tr.wp_banner_class_customization").show();
 
         } else if ($("input#predefined").is(':checked')) {
 
+            $("tr.wp_banner_class_managing").show();
             $("tr.wp_banner_class_templates").show();
             $("tr.wp_banner_class_customization").hide();
         } else if ($("input#none").is(':checked')) {
+
             $("tr.wp_banner_class_managing").hide();
             $("tr.wp_banner_class_templates").hide();
             $("tr.wp_banner_class_customization").hide();
         } else {
-
             $("tr.wp_banner_class_templates").hide();
             $("tr.wp_banner_class_customization").hide();
         }
